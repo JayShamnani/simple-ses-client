6df3e7c9bc631c0dce8294be25d72e56b2ccacdc
JayShamnani <shamnani.jay@gmail.com>
2022-05-19 11:26:01 +0530

    template for apis added

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,12 +1,12 @@
 <template>
-  <header class="p-3 mb-3 border-bottom">
+  <header class="mb-1 border-bottom">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
           <img :src="logo" class="logo">
         </a>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <ul class="nav col-lg-auto me-lg-auto justify-content-center mb-md-0">
 
         </ul>
 
@@ -15,9 +15,8 @@
             <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
           </a>
           <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
             <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Sign out</a></li>
           </ul>
@@ -39,8 +38,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .logo {
     width: 150px;
+}
+.dropdown {
+  margin-left: 10px;
+}
+
+header {
+  background-color: #111;
+  box-shadow: 0 1px 2px rgba(0,0,0,.1);
+  z-index: 1;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,46 @@
+import axios from 'axios';
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    userTokens: {},
+    userMails: {},
+    userProfile: {},
   },
   getters: {
-  },
-  mutations: {
+    userTokens: state => state.userTokens,
+    userMails: state => state.userMails,
+    userProfile: state => state.userProfile,
   },
   actions: {
+    async signIn({ commit }, { email, password }) {
+      const response = await axios.post('/api/sign-in', { email, password });
+      commit('setUserTokens', response.data);
+    },
+    async signUp({ commit }, { email, password, is_admin }) {
+      const response = await axios.post('/api/create-user', { email, password, is_admin });
+      commit('setUserTokens', response.data);
+    },
+    async respondToAuthChallenge({ commit }, { access_token, email, challenge_name, challenge_response }) {
+      const response = await axios.post('/api/respond-to-auth-challenge', { access_token, email, challenge_name, challenge_response });
+      commit('setUserTokens', response.data);
+    },
+    async changePassword({ commit }, { access_token, old_password, new_password }) {
+      const response = await axios.post('/api/change-password', { access_token, old_password, new_password });
+      commit('setUserTokens', response.data);
+    },
+    async getUserProfile({ commit }, { access_token }) {
+      const response = await axios.post('/api/get-user-profile', { access_token });
+      commit('setUserProfile', response.data);
+    },
+    async getEmails({ commit }, { access_token }) {
+      const response = await axios.post('/api/get-emails', { headers: { access_token } });
+      commit('setUserMails', response.data);
+    },
+  },
+  mutations: {
+    setUserTokens: (state, userTokens) => (state.userTokens = userTokens),
+    setUserMails: (state, userMails) => (state.userMails = userMails),
   },
   modules: {
   }
